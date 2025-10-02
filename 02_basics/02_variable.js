deaee1e5b56958df10847051d85550735abbe98c
luckygautam2009-alt <luckygautam2009@gmail.com>
2025-10-02 17:00:46 +0000

    commit changes

---
+++ 02_basics/02_variable.js
@@ -0,0 +1,18 @@
+const accountId = 43214;
+let accountEmail = "totu@gmail.com"
+let accountpassword = "54321";
+let accountcity = "dadri";
+let accountstate;
+// accountId = 4// not allowed
+
+accountEmail = "ballu@gmail.com";
+accountpassword = "6574"
+accountcity = "noida";
+
+
+
+console.table([,accountEmail,,accountpassword,accountcity,accountstate]);
+
+
+
+
