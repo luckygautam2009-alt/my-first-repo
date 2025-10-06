60b2c40ded6f7f824983281a837e4eb4b12b6abf
luckygautam2009-alt <luckygautam2009@gmail.com>
2025-10-06 23:23:33 +0000

    datatypes

--- 02_basics/02_datatypes.js
+++ 02_basics/02_datatypes.js
@@ -1,3 +1,17 @@
-"use strict"
+"use strict"; //treat all code as newer version//
+
+//alert(3+3)// not allowed we are using nodejs, not browser
+// code readability should be high //
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => mtlb khaali hai or standalone value
+// undefined => variable described but value define
+// symbol => unique
+
 console.log(3+3);
-console.log(typeof raja);
+console.log("Yash");
+console.log(typeof undefined);// undefined
+console.log(typeof null);//object
--- 02_basics/02_variable.js
+++ 02_basics/02_variable.js
@@ -3,6 +3,10 @@ let accountEmail = "totu@gmail.com"
 let accountpassword = "54321";
 let accountcity = "dadri";
 let accountstate;
+
+/*
+prefer not to use var because of issues in block scope and functional scope */
+
 // accountId = 4// not allowed
 
 accountEmail = "ballu@gmail.com";
