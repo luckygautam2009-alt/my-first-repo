badb2f1ea353c5e0341e737289abae1eb552ebca
luckygautam2009-alt <luckygautam2009@gmail.com>
2025-10-07 00:50:42 +0000

    conversion of datatypes

---
+++ 02_basics/02_conversionoperators.js
@@ -0,0 +1,29 @@
+let score = false;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "50" => 50
+// "55abc" => NaN
+// true => 1; false => 0
+
+let isloggedIn = "Yash Gautam";
+
+
+let booleanIsloggedIn = Boolean(isloggedIn);
+// console.log(booleanIsloggedIn);
+
+// 1 => true
+// 0 => false
+//"" => false
+// "Yash Gautam" => true
+
+let anynumber = 66;
+let stringNumber = String(anynumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
