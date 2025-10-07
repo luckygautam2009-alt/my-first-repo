dff626ad4e5e94c5919f05418eb31d32561088fb
luckygautam2009-alt <luckygautam2009@gmail.com>
2025-10-07 01:44:07 +0000

    new changes

--- 02_basics/02_conversionoperators.js
+++ 02_basics/02_conversionoperators.js
@@ -16,7 +16,7 @@ let isloggedIn = "Yash Gautam";
 
 
 let booleanIsloggedIn = Boolean(isloggedIn);
-// console.log(booleanIsloggedIn);
+//console.log(booleanIsloggedIn);
 
 // 1 => true
 // 0 => false
@@ -25,5 +25,40 @@ let booleanIsloggedIn = Boolean(isloggedIn);
 
 let anynumber = 66;
 let stringNumber = String(anynumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ****************** Operations **************** //
+
+let value = 10;
+let negvalue = String(-value);
+// console.log(negvalue);
+// console.log(typeof negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);** defines power
+// console.log(2/2);
+// console.log(2%2);give remainder
+
+let str1 = "hello";
+let str2 = " lucky";
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log("2"+3+4);
+console.log(2+4+"3");
+console.log((4+6)*2%3);
+
+// we can add 2 strings it doesn't add airthmatically it only merge/combine both strings //
+
+// +true possible but true+ gives an error//
+
+let successor = 150;
+++successor;
+console.log(successor);
+let predcessor = 201;
+--predcessor;
+console.log(predcessor);
